e780e2f700f4327529499c7ad7a0802426ebf1f0
Tiger-raj <rajbajpai2004@gmail.com>
2024-06-10 16:25:17 +0530

    add functionality to the signup page

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    proxy: {
+      "/api": {
+        target: "http://localhost:3000",
+        secure: false,
+      },
+    },
+  },
+  // this is added cuz our frontend runs at port 5173 while our backend runs at 3000, so if we need to handle any backend route, we need to change port to 3000, so we had made all backend routes to start with /api route then a / followed by specific routes, so whenever /api is present in route, target will be changed to point to port 3000
   plugins: [react()],
-})
+});
