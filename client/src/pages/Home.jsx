4fac7eecc41615d65587b5e3e33e59d7cd2492b8
Tiger-raj <rajbajpai2004@gmail.com>
2024-06-18 23:40:49 +0530

    added search page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -30,7 +30,7 @@ export default function Home() {
             <h2 className="text-2xl font-semibold text-center">Recent Posts</h2>
             <div className="flex flex-wrap gap-4">
               {posts.map((post) => (
-                <PostCard key={post.id} post={post} />
+                <PostCard key={post._id} post={post} />
               ))}
             </div>
             <Link to="/search" className="text-lg text-center text-teal-500  hover:underline">
