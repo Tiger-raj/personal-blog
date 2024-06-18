3a0a08edf8b331ff20968210805051718229de15
Tiger-raj <rajbajpai2004@gmail.com>
2024-06-19 00:11:15 +0530

    completed about and project pages

--- client/src/pages/Projects.jsx
+++ client/src/pages/Projects.jsx
@@ -1,3 +1,11 @@
+import CallToAction from "../component/CallToAction.jsx";
+
 export default function Projects() {
-  return <div>Projects</div>;
+  return (
+    <div className="min-h-screen max-w-2xl flex mx-auto flex-col justify-center items-center gap-6 p-3 ">
+      <h1 className="text-3xl font-semibold">Projects</h1>
+      <p className="text-md text-gray-500">Build fun and engaging projects while learning web development.</p>
+      <CallToAction />
+    </div>
+  );
 }
